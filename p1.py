bf5a120e8cdd82051b2c37058b8e513430e375be
immanuelsavio <30970161+immanuelsavio@users.noreply.github.com>
2018-06-16 12:29:11 +0400

    Update p1.py

--- p1.py
+++ p1.py
@@ -5,12 +5,12 @@ import numpy as np
 
 digits = datasets.load_digits()
 
-clf = svm.SVC(gamma=0.001, C=100)
+clf = svm.SVC(gamma=0.001, C=100) #increase gamma for more accuracy
 
-x,y = digits.data[:-10], digits.target[:-10]
+x,y = digits.data[:-10], digits.target[:-10] #dont Edit this number 
 clf.fit(x,y)
 
-print('Prediction:',clf.predict(digits.data[[-2]]))
+print('Prediction:',clf.predict(digits.data[[-2]]))#Edit this number for more examples
 
 plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation="nearest")
 plt.show()
