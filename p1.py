e582832c92aa0c61a7d233030a6e80b270e11280
immanuelsavio <immanuelsavio@gmail.com>
2018-06-16 12:16:24 +0400

    the basic image recog is done

--- p1.py
+++ p1.py
@@ -5,4 +5,12 @@ import numpy as np
 
 digits = datasets.load_digits()
 
-print(digits.data)
+clf = svm.SVC(gamma=0.001, C=100)
+
+x,y = digits.data[:-10], digits.target[:-10]
+clf.fit(x,y)
+
+print('Prediction:',clf.predict(digits.data[[-2]]))
+
+plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.show()
